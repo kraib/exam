db6528fa9bc144c4bfb61d9cf93316df5ac1b8bf
mark <lubegamark@gmail.com>
2015-09-05 09:38:03 +0300

    Removed config/db.php

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=exam',
-    'username' => 'root',
-    'password' => 'rs4',
-    'charset' => 'utf8',
-];
